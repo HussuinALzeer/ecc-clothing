c9f1451a23dac4876f19e1b25d278e00617594b3
HussuinALzeer <100776597+HussuinALzeer@users.noreply.github.com>
2022-11-22 21:35:35 +0100

    idk

--- src/component/cart-icon/cart-icon.component.jsx
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -9,19 +9,29 @@ import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag.svg";
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({toggleCartHiddenName})=>{
+const CartIcon = ({toggleCartHiddenName,itemCount})=>{
 
     return(
         <div className="cart-icon" onClick={toggleCartHiddenName}>
             <ShopingIcon className="shopping-icon"></ShopingIcon>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
 
 
-//redux
+//redux to connect it with the action
 const mapDispatchToProps = dispatch =>({
     toggleCartHiddenName: ()=> dispatch(toggleCartHidden())
 })
-export default connect(null,mapDispatchToProps)(CartIcon);
+
+
+// redux to bring the data from redux
+//i will add a new object to the list togather all quantity
+const mapStatuToProps = ({cart :{cartItems}}) =>({
+    itemCount :cartItems.reduce(
+        (accumalatedQuantity, cartItem) =>accumalatedQuantity + cartItem.quantity 
+        , 0)    // reduce here is a native array method in javascript
+})
+
+export default connect(mapStatuToProps,mapDispatchToProps)(CartIcon);
